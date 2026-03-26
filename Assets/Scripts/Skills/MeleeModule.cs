c979363fa346a46102798899dcc7c529b422ddd9
D05em10 <68335288+D05em10@users.noreply.github.com>
2026-03-26 17:04:19 +0000

    w
    w

---
+++ Assets/Scripts/Skills/MeleeModule.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSkillModule", menuName = "Skill/SkillModule")]
+public class MeleeModule : ScriptableObject
+{
+    public Vector3 hitboxSize = new Vector3(1, 1, 1);
+    public float duration = 0.2f;
+    public int damage = 10;
+    
+}
