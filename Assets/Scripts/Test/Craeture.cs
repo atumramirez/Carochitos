b24f46e88f1c7c3eaa1cfa857fddf060a2e2fa8e
D05em10 <68335288+D05em10@users.noreply.github.com>
2026-04-09 12:03:25 +0100

    w

--- Assets/Scripts/Test/Craeture.cs
+++ Assets/Scripts/Test/Craeture.cs
@@ -5,9 +5,26 @@ public class Creature : MonoBehaviour
     public string creatureName;
 
     public Carochito carochito;
+    public int maxHp;
+    public int currentHp;
+    public int damage;
 
     public void Capture()
     {
         Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        maxHp = carochito.CurrentHP;
+        currentHp = maxHp;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Attack")
+        {
+            currentHp = currentHp - damage;
+        }
     }
 }
